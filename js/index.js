045edf7e5e5ffb50ff50f20161ca1905ea986dd7
Andy Garcia <andygarcia@live.fr>
2021-03-03 16:50:22 +0100

    ajout de la fonction insertColor

--- js/index.js
+++ js/index.js
@@ -25,6 +25,36 @@ function insertImageUrl(section, imageTeddies){
     newImg.setAttribute("src", "teddy picture - photo ourson")
 }
 
+function insertName(div, nameTeddies){
+    const newH3 = document.createElement("h3");
+    div.appendChild(newH3);
+    newH3.innerHTML = nameTeddies; 
+}
+
+function insertId(div, idTeddies){
+    const newDiv2 = document.createElement("h3");
+    div.appendChild(newDiv2);
+    newDiv2.className = "";
+    const newP1 = document.createElement("p");
+    newDiv2.appendChild(newP1);
+    const newSpan = document.createElement("span");
+    newP1.appendChild(newSpan);
+    newSpan.innerHTML = "Numero d'id : ";
+    const newP2 = document.createElement("p");
+    newDiv2.appendChild(newP2);
+    newP2.className = "";
+    newP2.innerHTML = idTeddies;
+}
+
+
+function insertColor(div){
+    const newP3 = document.createElement("p");
+    div.appendChild(newP3);
+    const newSpan2 = document.createElement("span");
+    newP3.appendChild(newSpan2);
+    newSpan2.innerHTML = "Couleur : Modifiable";
+}
+
 
     
 
